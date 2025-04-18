cdf8f96226aaf7560f051037aae0f2697cacc170
Wallace Gabriel <gs008963@gmail.com>
2025-04-18 01:33:53 +0000

    Adiciona manipulador de exceções global para tratamento de erros em controladores

---
+++ src/main/java/com/wgabbriel/carpooling/config/exception/handler/RestExceptionHandler.java
@@ -0,0 +1,27 @@
+package com.wgabbriel.carpooling.config.exception.handler;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class RestExceptionHandler {
+
+  @ExceptionHandler(IllegalArgumentException.class)
+  public ResponseEntity<String> handleIllegalArgumentException(IllegalArgumentException e) {
+    return ResponseEntity.status(400).body(e.getMessage());
+  }
+
+  @ExceptionHandler(UsernameNotFoundException.class)
+  public ResponseEntity<String> handleUsernameNotFoundException(UsernameNotFoundException e) {
+    return ResponseEntity.status(404).body(e.getMessage());
+  }
+
+  @ExceptionHandler(MethodArgumentNotValidException.class)
+  public ResponseEntity<String> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+    return ResponseEntity.status(400)
+        .body(e.getAllErrors().get(0).getDefaultMessage());
+  }
+}
