611127c3058cae864528bb90605c795b1187c4e6
Wallace Gabriel <gs008963@gmail.com>
2025-04-18 01:33:14 +0000

    Adiciona configuração de segurança e controle de autenticação, incluindo registro de usuários

---
+++ src/main/java/com/wgabbriel/carpooling/controller/AuthController.java
@@ -0,0 +1,30 @@
+package com.wgabbriel.carpooling.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.wgabbriel.carpooling.entity.User;
+import com.wgabbriel.carpooling.service.AuthService;
+
+import jakarta.validation.Valid;
+
+@RestController
+@RequestMapping("/api/v1/auth")
+public class AuthController {
+
+  private final AuthService authService;
+
+  public AuthController(AuthService authService) {
+    this.authService = authService;
+  }
+
+  @PostMapping("/register")
+  public ResponseEntity<User> register(@RequestBody @Valid User user) {
+    return ResponseEntity.status(HttpStatus.CREATED).body(authService.create(user));
+  }
+
+}
